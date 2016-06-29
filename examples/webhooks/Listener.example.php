e854f9ca85fe7d4b65a44b52db1ee6fa38c8cd6d
Kuba Pilecki <k.pilecki@gmail.com>
2016-06-29 16:21:47 +0200

    Aero-1705

--- examples/webhooks/Listener.example.php
+++ examples/webhooks/Listener.example.php
@@ -8,6 +8,7 @@ require_once(dirname(__FILE__)."/../config.php");
 require_once(dirname(__FILE__)."/../../contractors/ContractorsApiWebHooks.class.php");
 require_once(dirname(__FILE__)."/../../transactions/TransactionsApiWebHooks.class.php");
 require_once(dirname(__FILE__)."/../../protection/ProtectionApiWebHooks.class.php");
+require_once(dirname(__FILE__)."/../../liabilities/LiabilitiesApiWebHooks.class.php");
 
 Logger::setWriter(new FileLoggerWriter(dirname(__FILE__). '/Listener.log.txt'));
 
@@ -16,8 +17,14 @@ $client = new WebHooksClient(INVIPAY_PARTNER_API_KEY, INVIPAY_PARTNER_SIGNATURE_
 $client->onEvent(ContractorsApiWebHooks::getDefinition('ContractorVerificationCompletedEvent'), 'onContractorVerificationCompletedEvent');
 $client->onEvent(ContractorsApiWebHooks::getDefinition('AccountCreationCompletedEvent'), 'onAccountCreationCompletedEvent');
 $client->onEvent(ContractorsApiWebHooks::getDefinition('AccountProfileChangedEvent'), 'onAccountProfileChangedEvent');
+
 $client->onEvent(TransactionsApiWebHooks::getDefinition('TransactionDeletedEvent'), 'onTransactionDeletedEvent');
+
 $client->onEvent(ProtectionApiWebHooks::getDefinition('TransactionProtectionCompletedEvent'), 'onTransactionProtectionCompletedEvent');
+
+$client->onEvent(LiabilitiesApiWebHooks::getDefinition('NewCommissionInvoiceCreatedEvent'), 'onNewCommissionInvoiceCreatedEvent');
+$client->onEvent(LiabilitiesApiWebHooks::getDefinition('NewInterestInvoiceCreatedEvent'), 'onNewInterestInvoiceCreatedEvent');
+$client->onEvent(LiabilitiesApiWebHooks::getDefinition('NewInterestNoteCreatedEvent'), 'onNewInterestNoteCreatedEvent');
 
 $client->handle();
 
@@ -44,6 +51,21 @@ function onTransactionDeletedEvent($data)
 function onTransactionProtectionCompletedEvent($data)
 {
 	Logger::info('Transaction protection completed with data: {0}', $data);
+}
+
+function onNewCommissionInvoiceCreatedEvent($data)
+{
+	Logger::info('Commission invoice created with data: {0}', $data);
+}
+
+function onNewInterestInvoiceCreatedEvent($data)
+{
+	Logger::info('Interest invoice created with data: {0}', $data);
+}
+
+function onNewInterestNoteCreatedEvent($data)
+{
+	Logger::info('Interest note created with data: {0}', $data);
 }
 
 ?>
